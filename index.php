483952e4cec94d4751ab496fd7992226ba993672
Razvan Smarandeanu <razvansg@yahoo.com>
2015-11-07 11:53:20 +0200

    added facebook php sdk

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-Happy City :)
+<?php
+
+require_once __DIR__ . '/Facebook/autoload.php';
+
+
+
+$fb = new Facebook\Facebook([
+  'app_id' => '177111075965835',
+  'app_secret' => '3bc9924659d9349e51f52597b277a8e7',
+  'default_graph_version' => 'v2.5',
+  ]);
+
+session_start();
+
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['email', 'user_likes']; // optional
+$loginUrl = $helper->getLoginUrl('http://happycity.xyz/login-callback.php', $permissions);
+
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
