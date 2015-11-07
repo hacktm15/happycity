5d766b5e3adbd49e654f6de9b40826b2ba996fea
Alin Tuhut <alin.tuhut@gmail.com>
2015-11-07 21:19:57 +0200

    survey love

--- application/views/survey/thankyou.php
+++ application/views/survey/thankyou.php
@@ -1,1 +1,17 @@
-<h2>Multumim frumos!</h2>
+
+<?php $this->view('includes/header'); ?>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-xs-12">
+                <h2>Multumim frumos!</h2>
+                <div class="fb-share-button" data-href="https://<?= $_SERVER['HTTP_HOST']?>/index.php/survey/" data-layout="button_count"></div>
+            </div>
+
+        </div>
+
+    </div>
+
+<?php $this->view('includes/footer'); ?>
