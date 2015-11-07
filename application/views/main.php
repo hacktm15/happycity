81d26bf6b4c9a4367e3e3aa37ce6151deb449633
Alin Tuhut <alin.tuhut@gmail.com>
2015-11-07 14:36:50 +0200

    initial bootstrap based layout

--- application/views/main.php
+++ application/views/main.php
@@ -1,85 +1,81 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+<?php include('includes/header.php'); ?>
 
-	<style type="text/css">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Happy City</a>
+        </div>
+      </div>
+    </nav>
+    <div class="jumbotron">
+      <div class="container">
+        <div class="embed-responsive embed-responsive-4by3">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0yMZ-04ud_Q" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <p>   </p>
+        <p>It only takes one minute to complete the survey.</p>
+        <p><a class="btn btn-primary btn-lg" href="survey.html" role="button">Take the survey! &raquo;</a></p>
+      </div>
+    </div>
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-6"><h2>Timisoara</h2></div>
+            <div class="col-md-6"></div>
+          </div>
+        <div class="embed-responsive embed-responsive-4by3">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0yMZ-04ud_Q" frameborder="0" allowfullscreen></iframe>
+        </div>
+        </div>
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-6"><h2>Bucuresti</h2></div>
+            <div class="col-md-6"></div>
+          </div>
+        <div class="embed-responsive embed-responsive-4by3" >
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0yMZ-04ud_Q" frameborder="0" allowfullscreen></iframe>
+        </div>
+       </div>
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-6"><h2>Cluj</h2></div>
+            <div class="col-md-6"></div>
+          </div>     
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+         <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-6"><h2>Bacau</h2></div>
+            <div class="col-md-6"></div>
+          </div>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+      <hr>
+      <footer>
+        <p>&copy; Happy City</p>
+      </footer>
+    </div> <!-- /container -->
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
 
-	#body {
-		margin: 0 15px 0 15px;
-	}
 
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
 		<?php if (@$userData['first_name']) { ?>
 			Salut <?= $userData['first_name'] ?>, <?= $userData['location']['name'] ?>
 		<?php } else { ?>
 			<a href="<?= $loginUrl ?>">Login with Facebook</a>
 		<?php } ?>		
-	</div>
+	
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
 
-</body>
-</html>
+<?php include('includes/footer.php'); ?>
