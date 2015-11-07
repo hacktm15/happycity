01d72cc30f6b7ed74807e8eb6a435e6048ca9984
Alin Tuhut <alin.tuhut@gmail.com>
2015-11-08 00:17:50 +0200

    it's late

--- application/views/includes/credits.php
+++ application/views/includes/credits.php
@@ -2,7 +2,7 @@
 <footer>
 	<div class="row">
 		<div class="col-md-6">
-			<p>&copy; Happy City &midot; <a href="/welcome/privacy">Privacy</a></p>
+			<p>&copy; Happy City &middot; <a href="/welcome/privacy">Privacy</a></p>
 		</div>
 		<div class="col-md-6">
 			<p class="text-right">made with ♥ at <a href="http://hacktm.ro">HackTM 2015</a></p>
