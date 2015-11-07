83af66722aaf0b50e6a6dc735474a9fc4289002c
Alin Tuhut <alin.tuhut@gmail.com>
2015-11-07 20:57:23 +0200

    different styles for logged users

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -60,15 +60,17 @@
 		<img src="/assets/img/logo.png" alt="Happy City">
           </a>
         </div>
-        <div class="navbar-right">
-		<p class="welcome">
+        <div class="navbar-right">		
 			<?php if (@$userData['first_name']) { ?>
+            <p class="welcome welcome-logged">
                 <img src="<?= $userData['picture']['data']['url'] ?>"> <?= $userData['first_name'] ?>, bine ai venit!
-            <?php } else { ?>
-                
-                <a href="<?= $loginUrl ?>"><i class="fa fa-facebook-square"></i><b>Login cu Facebook</b></a>
-            <?php } ?>          
             </p>
+            <?php } else { ?>
+            <p class="welcome">
+                <a href="<?= $loginUrl ?>"><i class="fa fa-facebook-square"></i><b>Login cu Facebook</b></a>
+            </p>
+            <?php } ?>          
+            
         </div>
       </div>
     </nav>
